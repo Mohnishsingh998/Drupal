4f1d0f3b8b0ec4795d68a0892de7c3a084629b21
mohnishsingh yadav <132808264+Mohnishsingh998@users.noreply.github.com>
2026-03-30 15:12:34 +0530

    Made my Own custom theme Named it Mohnish

---
+++ custom_theme_exploration/web/themes/custom_theme_exploration/js/custom-theme-exploration.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Custom Theme Exploration behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.customThemeExploration = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
---
+++ custom_theme_exploration/web/themes/mohnish/js/mohnish.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * mohnish behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.mohnish = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
